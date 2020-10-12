8d71e4b9cc5e16c4df039cc943f52861d6b444fb
Ryan Connaughton <hello@ryanconnaughton.com>
2020-10-12 11:17:46 +0100

    Updated responsive / mobile design

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Turns header/nav secondary CTA into primary CTA when scrolled
 window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 300) {
-        document.getElementById('sign-up').style.cssText = 'background: blue; color: white !important; transition: 0.5s;';
+    if (window.pageYOffset > 500) {
+        document.getElementById('sign-up').style.cssText = 'background: #1477FF; color: white !important; transition: 0.5s;';
     } else {
         document.getElementById('sign-up').style = 'transition: 0.5s'; // add transition to all css
     }
