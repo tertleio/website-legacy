19e72f7fc735551b7c748a1f7bf801c957ba36c6
Ryan Connaughton <hello@ryanconnaughton.com>
2020-10-25 19:42:01 +0000

    Backup before refactoring nav

--- public/js/main.js
+++ public/js/main.js
@@ -35,7 +35,7 @@ signupForm.addEventListener('submit', (submit) => {
     submitData(formData, signupSubmitUrl);
 });
 
-// NAVIGATION
+// NAV ----------------------------------------------
 
 const header = document.getElementById('header');
 const logo = document.getElementById('logo');
@@ -53,56 +53,3 @@ window.addEventListener('scroll', () => {
             }
     }
 });
-
-
-// RESPONSIVE LOGOS
-function responsiveChanges() {
-    let width = window.innerWidth;
-    
-    if (width < 800) {
-        if (header.className == 'wrap') {
-            logo.src='./img/logo-short-dark.svg';
-        }
-        navBtn.className='btn btn-secondary burger';
-        navBtn.innerHTML='Menu';
-        return true;
-    } else {
-        logo.src='./img/logo-dark.svg';
-        navBtn.className='btn btn-secondary';
-        navBtn.innerHTML='Join Waitlist';
-        return false;
-    }
-} 
-
-function navMenu () {
-    if (responsiveChanges()) {
-        logo.src='./img/logo-short-light.svg';
-        if (header.className == 'wrap') {
-            header.className='wrap menu';
-            navBtn.innerHTML='close';
-            logo.src='./img/logo-short-light.svg';
-        } else {
-            header.className='wrap';
-            logo.src='./img/logo-short-dark.svg';
-            navBtn.innerHTML='Menu';
-        }
-    }
-}
-
-
-// window.onresize = responsiveChanges;
-window.addEventListener('load', responsiveChanges);
-window.addEventListener('resize', responsiveChanges);
-navBtn.addEventListener('click', navMenu);
-
-// CLOSE MENU WHEN CLICKING AN ANCHOR LINK --fix duplicate functions
-
-navLink[0].addEventListener('click', () => {
-    navMenu();
-});
-
-navLink[1].addEventListener('click', () => { 
-    navMenu();
-});
-
-};
