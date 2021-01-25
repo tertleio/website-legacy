ba05b68ee10a0565affa17034780b7a26b628824
Ryan Connaughton <hello@ryanconnaughton.com>
2021-01-25 18:45:08 +0000

    Final commit before push to prod

--- middleware/forceHttps.js
+++ middleware/forceHttps.js
@@ -1,6 +1,5 @@
 const forceHttpsIfProd = async (req, res, next) => {
   if (!req.headers.host.includes('staging') && !req.secure) {
-    console.log(req.secure);
     return res.redirect('https://' + req.headers.host + req.url);
   }
   next();
