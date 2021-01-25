d01d5c4e6dec726fd43140275ad943585f6e1138
Ryan Connaughton <hello@ryanconnaughton.com>
2021-01-25 19:12:38 +0000

    Added enable trust proxy and retesting

--- middleware/forceHttps.js
+++ middleware/forceHttps.js
@@ -1,8 +1,6 @@
 const forceHttpsIfProd = async (req, res, next) => {
-  console.log('this ran');
-  if (!req.headers.host.includes('staging') && !req.secure) {
+  if (!req.secure) {
     console.log('inside if ran');
-    // return res.redirect('http://' + req.headers.host + '/hello');
     return res.redirect('https://' + req.headers.host + req.url);
   }
   next();
