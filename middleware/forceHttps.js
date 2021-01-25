f6a15c0bc1206dca41571e3531ab5e222f037a0b
Ryan Connaughton <hello@ryanconnaughton.com>
2021-01-25 19:05:05 +0000

    Retrying

--- middleware/forceHttps.js
+++ middleware/forceHttps.js
@@ -1,6 +1,10 @@
+const { networkInterfaces } = require('os');
+
 const forceHttpsIfProd = async (req, res, next) => {
   console.log('this ran');
   if (!req.headers.host.includes('staging') && !req.secure) {
+    console.log('inside if ran');
+    // return res.redirect('http://' + req.headers.host + '/hello');
     return res.redirect('https://' + req.headers.host + req.url);
   }
   next();
