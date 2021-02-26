bd0eff4498f3cd94f669010f3c6f895d3c061ed4
Ryan Connaughton <hello@ryanconnaughton.com>
2021-02-26 15:04:49 +0000

    Fixed https to https forwarder

--- middleware/forceHttps.js
+++ middleware/forceHttps.js
@@ -1,9 +1,18 @@
 const forceHttpsIfProd = async (req, res, next) => {
-  if (!req.headers.host.includes('staging') && !req.secure) {
-    console.log('inside if ran');
-    return res.redirect('https://' + req.headers.host + req.url);
+  if (
+    req.headers.host.includes('localhost') ||
+    req.headers.host.includes('staging')
+  )
+    return next();
+  // (!!) set env variables instead here
+  else {
+    if (!req.secure) {
+      console.log('inside if ran');
+      return res.redirect('https://' + req.headers.host + req.url);
+    } else {
+      return next();
+    }
   }
-  next();
 };
 
 module.exports = { forceHttpsIfProd };
