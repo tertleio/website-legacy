2037d83b8cf047e2173ac66ef4ae281c266b4dae
Ryan Connaughton <hello@ryanconnaughton.com>
2021-02-26 15:28:39 +0000

    Https fix attempt 2

--- middleware/forceHttps.js
+++ middleware/forceHttps.js
@@ -2,13 +2,14 @@ const forceHttpsIfProd = async (req, res, next) => {
   if (
     req.headers.host.includes('localhost') ||
     req.headers.host.includes('staging')
-  )
+  ) {
     return next();
-  // (!!) set env variables instead here
-  else {
+    // (!!) set env variables instead here
+  } else {
     if (!req.secure) {
       console.log('inside if ran');
-      return res.redirect('https://' + req.headers.host + req.url);
+      res.redirect('https://' + req.headers.host + req.url);
+      return next();
     } else {
       return next();
     }
