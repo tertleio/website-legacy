0020b41710f90dda229a2069e9043e49845f303d
Ryan Connaughton <hello@ryanconnaughton.com>
2022-07-11 17:27:02 +0100

    desktop css clean up

--- public/js/nav.js
+++ public/js/nav.js
@@ -4,67 +4,14 @@ const logo = document.getElementById('logo');
 const navBtn = document.getElementById('nav-btn');
 const navLink = document.querySelectorAll('nav a');
 
-
-// Turns header/nav secondary CTA into primary CTA when scrolled
 const nav = () => {
-    window.addEventListener('scroll', () => {
-        if (window.pageYOffset > 500 && header.className == 'wrap') {
-            navBtn.className='btn btn-primary';
-        } else {
-            navBtn.className= 'btn btn-secondary'; // add transition to all css
-            }
-    });
-}
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 500 && header.className == 'wrapper') {
+      navBtn.className = 'btn btn--primary';
+    } else {
+      navBtn.className = 'btn btn--secondary';
+    }
+  });
+};
 
 export default nav;
-
-/*
-// RESPONSIVE LOGOS
-function responsiveChanges() {
-    let width = window.innerWidth;
-    
-    if (width < 800) {
-        if (header.className == 'wrap') {
-            logo.src='./img/logo-short-dark.svg';
-        }
-        navBtn.className='btn btn-secondary burger';
-        navBtn.innerHTML='Menu';
-        return true;
-    } else {
-        logo.src='./img/logo-dark.svg';
-        navBtn.className='btn btn-secondary';
-        navBtn.innerHTML='Join Waitlist';
-        return false;
-    }
-} 
-
-function navMenu () {
-    if (responsiveChanges()) {
-        logo.src='./img/logo-short-light.svg';
-        if (header.className == 'wrap') {
-            header.className='wrap menu';
-            navBtn.innerHTML='close';
-            logo.src='./img/logo-short-light.svg';
-        } else {
-            header.className='wrap';
-            logo.src='./img/logo-short-dark.svg';
-            navBtn.innerHTML='Menu';
-        }
-    }
-}
-
-
-// window.onresize = responsiveChanges;
-window.addEventListener('load', responsiveChanges);
-window.addEventListener('resize', responsiveChanges);
-navBtn.addEventListener('click', navMenu);
-
-// CLOSE MENU WHEN CLICKING AN ANCHOR LINK --fix duplicate functions
-navLink[0].addEventListener('click', () => {
-    navMenu();
-});
-
-navLink[1].addEventListener('click', () => {
-    navMenu();
-});
-*/
