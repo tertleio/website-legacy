f12bd39f0d0bf9acda8e0831162fd29c8a2bcb48
Ryan Connaughton <hello@ryanconnaughton.com>
2022-07-14 16:43:12 +0100

    finalised menu drop down styling

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,23 +2,23 @@
 const doc = document;
 
 function nav() {
-  const burger = doc.querySelector('.burger');
+  const burger = doc.querySelector('.burger-menu');
   const lines = doc.querySelectorAll('.burger-line');
   const menu = doc.querySelector('.menu');
   let show = false;
 
   console.log(lines);
 
-  burger.addEventListener('click', (e) => {
-    console.log('menu clicked');
-    if (!show) {
-      menu.style = 'display: block';
-      show = true;
-    } else {
-      menu.style = 'display: none';
-      show = false;
-    }
-  });
+  // burger.addEventListener('click', (e) => {
+  //   console.log('menu clicked');
+  //   if (!show) {
+  //     menu.style = 'display: block';
+  //     show = true;
+  //   } else {
+  //     menu.style = 'display: none';
+  //     show = false;
+  //   }
+  // });
 }
 
 export default nav;
