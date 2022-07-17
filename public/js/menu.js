133e62cb64fe9b77dc1e48f4ca735341bef221c7
Ryan Connaughton <hello@ryanconnaughton.com>
2022-07-17 12:46:37 +0100

    header: added span icons to menu and progress on icon menu / removing js need

--- public/js/menu.js
+++ public/js/menu.js
@@ -5,19 +5,19 @@ const elBurger = doc.querySelector('.burger-menu');
 const elMenu = doc.querySelector('.menu');
 let show = false;
 
-function closeMenu(e) {
-  if (e.target.className.includes('burger')) return;
-  elMenu.style = 'display: none';
-  show = false;
-  window.removeEventListener('click', closeMenu);
-}
+// function closeMenu(e) {
+//   if (e.target.className.includes('burger')) return;
+//   elMenu.style = 'display: none';
+//   show = false;
+//   window.removeEventListener('click', closeMenu);
+// }
 
-function openMenu() {
-  elMenu.style = 'display: block';
-  if (!show) show = true;
-  if (show) window.addEventListener('click', closeMenu);
-}
+// function openMenu() {
+//   elMenu.style = 'display: block';
+//   if (!show) show = true;
+//   if (show) window.addEventListener('click', closeMenu);
+// }
 
-const menu = () => elBurger.addEventListener('click', openMenu);
+// const menu = () => elBurger.addEventListener('click', openMenu);
 
 export default menu;
