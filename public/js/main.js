60a204d5fc95234259b59cb315495bbf2c617e04
Ryan Connaughton <hello@ryanconnaughton.com>
2022-07-18 13:28:00 +0100

    minor js clean up and fixed content centered for tablets

--- public/js/main.js
+++ public/js/main.js
@@ -66,7 +66,6 @@ function onScroll() {
   }
 })();
 
-// menu();
 onScroll();
 init();
 
---
+++ public/js/utils/resizeCanvas.js
@@ -0,0 +1,24 @@
+(function () {
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+
+  // resize the canvas to fill browser window dynamically
+  window.addEventListener('resize', resizeCanvas, false);
+
+  function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    // canvas.height = window.innerHeight;
+
+    /**
+     * Your drawings need to be inside this function otherwise they will be reset when
+     * you resize the browser window and the canvas goes will be cleared.
+     */
+    drawStuff();
+  }
+
+  resizeCanvas();
+
+  function drawStuff() {
+    // do your drawing stuff here
+  }
+})();
