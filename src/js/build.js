75f10ca7de4fcda2df1920566088ff79187ce802
Ryan Connaughton <hello@ryanconnaughton.com>
2022-08-03 10:25:45 +0100

    bug: fixed components not loading by wrapping in async function (tablets don't support top level await)

--- src/js/build.js
+++ src/js/build.js
@@ -2,22 +2,18 @@
 const doc = document;
 import loadHtml from './utils/loadHtml.js';
 
-const headerHtml = await loadHtml('../components/header.xml', import.meta.url);
-const footerHtml = await loadHtml('../components/footer.xml', import.meta.url);
+const components = async () => {
+  const path = '../components';
+  const headerHtml = await loadHtml(`${path}/header.xml`, import.meta.url);
+  const footerHtml = await loadHtml(`${path}/footer.xml`, import.meta.url);
 
-// console.log(headerHtml);
-
-const initComponents = () => {
   const elHeader = doc.getElementById('header');
   const elFooter = doc.getElementById('footerlol');
-
-  // console.log(elFooter);
 
   elHeader.innerHTML = headerHtml;
   elFooter.innerHTML = footerHtml;
 };
 
-// Not widely supported yet
 // class Header extends HTMLElement {
 //   constructor() {
 //     super();
@@ -38,9 +34,9 @@ const initComponents = () => {
 //   }
 // }
 
-// const initComponents = () => {
+// const components = () => {
 //   customElements.define(`component-header`, Header);
 //   customElements.define(`component-footer`, Footer);
 // };
 
-export default initComponents;
+export default components;
