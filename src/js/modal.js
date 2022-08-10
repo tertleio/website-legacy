6d3c6618ce580cd6b0f2fa0544cb46ccc4d9cfbb
Ryan Connaughton <hello@ryanconnaughton.com>
2022-08-10 07:58:27 +0100

    modal: added open/close js and tweaked dropdown styles

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,22 +1,31 @@
 import loadHtml from './utils/loadHtml.js';
 
-async function openModal() {
+async function toggleModal() {
   const modalHtml = await loadHtml('../components/modal.html', import.meta.url);
   const elModal = document.querySelector('#overlay-modal');
-
+  // Open
+  elModal.style = '';
   elModal.innerHTML = modalHtml;
+
+  const elCloseBtn = document.querySelector('.modal-close');
+  // Close
+  elCloseBtn.addEventListener(
+    'click',
+    () => {
+      elModal.style.display = 'none';
+    },
+    { once: true }
+  );
 }
 
 async function submit(e) {
   e.preventDefault();
-  console.log('submitted email');
-  openModal();
+  toggleModal();
 }
 
 const ctrler = () => {
   const elSign = document.querySelector('.signup');
-  elSign.addEventListener('submit', submit);
-  openModal();
+  elSign.addEventListener('click', submit);
 };
 
 export default ctrler;
