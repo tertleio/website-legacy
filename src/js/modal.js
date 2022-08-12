02dc9ecaf2186d97267a16e23e86dd80f5ca176d
Ryan Connaughton <hello@ryanconnaughton.com>
2022-08-12 14:02:10 +0100

    modal: refactor to reuse functions - still ugly af though for something so simple

--- src/js/modal.js
+++ src/js/modal.js
@@ -13,76 +13,97 @@ function simFetch(vals) {
   });
 }
 
-async function handleModal(e, email) {
-  const modalHtml = await loadHtml('../components/modal.html', import.meta.url);
+const modal = () => {
   const elModal = doc.querySelector('#overlay-modal');
-
-  // Open
-  elModal.style = '';
-  elModal.innerHTML = modalHtml;
-  // TODO: focus modal for tabs and disable bg interactions
-
-  // Insert initial email if exists
-  if (email) {
-    const elEmail = elModal.querySelector('#email');
-    elEmail.value = email;
-  }
-
-  // Listen for second submit
-  const formTwo = elModal.querySelector('#formTwo');
-  formTwo.addEventListener('submit', onSecondarySubmit);
-
-  // Close
-  const elCloseBtn = doc.querySelector('.modal-close');
-  elCloseBtn.addEventListener(
-    'click',
-    () => {
-      elModal.style.display = 'none';
-    },
-    { once: true }
-  );
-}
-
-async function onSecondarySubmit(e) {
-  e.preventDefault();
-
-  const fields = e.target.querySelectorAll('.field');
-  const vals = [...fields].map((f) => ({ [f.name]: f.value }));
-
-  simFetch(vals)
-    .then((res) => {
-      const { status, payload } = res;
-      if (status !== 'success') throw new Error('Problem submitting data');
-
-      const elSuccess = doc.querySelector('#modal-success');
-      elSuccess.style = '';
-      // add id to local storage incase of error or resubmission
-      // update db to delete old and add new/correct
-    })
-    .catch((err) => {
-      console.log(err);
-      // notify user there was a problem
-    });
-
-  return false;
-}
-
-async function onInitialSubmit(e) {
-  e.preventDefault();
-  const email = e.target.querySelector('input').value;
-
-  handleModal(e, email);
-  const res = await simFetch(email);
-
-  return false;
-}
-
-const ctrler = () => {
   const formOne = doc.querySelector('#formOne');
   const elNavCta = doc.querySelector('.ctaOne');
+  formOne.addEventListener('submit', handleSubmit);
+  elNavCta.addEventListener('click', toggleModal);
 
-  formOne.addEventListener('submit', onInitialSubmit);
-  elNavCta.addEventListener('click', handleModal);
+  let isOpen = false;
+  let dataStruct = {
+    email: '',
+    firstName: '',
+    country: '',
+  };
+
+  async function toggleModal() {
+    const path = '../components/modal.html';
+    const modalHtml = await loadHtml(path, import.meta.url);
+
+    // Open
+    elModal.style = '';
+    elModal.innerHTML = modalHtml;
+
+    // Listen for submit
+    const formTwo = elModal.querySelector('#formTwo');
+    formTwo.addEventListener('submit', handleSubmit);
+
+    // Close
+    const elCloseBtn = doc.querySelector('.modal-close');
+    elCloseBtn.addEventListener(
+      'click',
+      () => {
+        elModal.style.display = 'none';
+      },
+      { once: true }
+    );
+
+    isOpen = !isOpen;
+  }
+
+  function getFormData(e) {
+    if (e.target.id === 'formOne') {
+      const email = e.target.querySelector('input').value;
+      console.log(email);
+      dataStruct.email = email;
+    } else if (e.target.id === 'formTwo') {
+      const fields = e.target.querySelectorAll('.field');
+      const vals = [...fields].map((f) => ({ [f.name]: f.value }));
+      dataStruct = vals;
+    }
+
+    return dataStruct;
+  }
+
+  async function submitFormData(data) {
+    return simFetch(data)
+      .then((res) => {
+        const { status, payload } = res;
+        if (status !== 'success') throw new Error('Problem submitting form');
+
+        return true;
+      })
+      .catch((err) => {
+        console.log(err);
+        // notify user there was a problem
+      });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (e.target.id === 'formOne') await toggleModal();
+    const formData = getFormData(e);
+    const wasSuccess = await submitFormData(formData);
+
+    // Ininitial email insertion to main form
+    if (e.target.id === 'formOne') {
+      const elEmail = elModal.querySelector('#email');
+      elEmail.value = formData.email;
+      return;
+    }
+
+    // User feedback
+    const elNotify = doc.querySelector(
+      `#modal-${wasSuccess ? 'success' : 'error'}`
+    );
+    elNotify.style = '';
+
+    // add id to local storage incase of error or resubmission
+    // update db to delete old and add new/correct
+
+    return false;
+  }
 };
 
-export default ctrler;
+export default modal;
