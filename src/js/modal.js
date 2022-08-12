24e3fc243bebb9e07911b380516d7648301c743e
Ryan Connaughton <hello@ryanconnaughton.com>
2022-08-12 08:59:59 +0100

    waitlist: add js to update second form with inital email sign

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,31 +2,46 @@
 const doc = document;
 import loadHtml from './utils/loadHtml.js';
 
-async function toggleModal() {
+async function toggleModal(email) {
   const modalHtml = await loadHtml('../components/modal.html', import.meta.url);
   const elModal = doc.querySelector('#overlay-modal');
   // Open
   elModal.style = '';
   elModal.innerHTML = modalHtml;
 
+  if (email) {
+    const elEmail = elModal.querySelector('#email');
+    elEmail.value = email;
+  }
+
   const elCloseBtn = doc.querySelector('.modal-close');
   // Close
-  // elCloseBtn.addEventListener(
-  //   'click',
-  //   () => {
-  //     elModal.style.display = 'none';
-  //   },
-  //   { once: true }
-  // );
+  elCloseBtn.addEventListener(
+    'click',
+    () => {
+      elModal.style.display = 'none';
+    },
+    { once: true }
+  );
 }
 
-function onSubmit(e) {
+function simFetch(email) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      return resolve({
+        status: 'success',
+        payload: email,
+      });
+    }, 100);
+  });
+}
+
+async function onSubmit(e) {
   e.preventDefault();
   const email = e.target.querySelector('input').value;
-  console.log('email', email);
 
-  // toggleModal();
-  // const res = fetch('https://app.tertle.io/api/sign/')
+  toggleModal(email);
+  const res = await simFetch(email);
 
   return false;
 }
