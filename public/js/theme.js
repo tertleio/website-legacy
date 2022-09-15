ff77279f6145c42dacaaac5b057560c3759d5801
Ryan Connaughton <hello@ryanconnaughton.com>
2022-09-15 10:22:47 +0100

    mv: index.html to public dir root & updated paths

--- public/js/theme.js
+++ public/js/theme.js
@@ -7,7 +7,7 @@ const toggleTheme = (showFooterVisual) => {
   const elRocket = doc.querySelector('.rocket');
 
   function updateFooter(theme) {
-    elRocket.src = `../assets/tertle_rocket-${theme}-sm.gif`;
+    elRocket.src = `./assets/tertle_rocket-${theme}-sm.gif`;
 
     if (!showFooterVisual) return;
     const elMatrix = doc.getElementById('matrix');
