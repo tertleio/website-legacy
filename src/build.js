dcab93d409a077906f4eb8678a9ac8ebd2daa931
Ryan Connaughton <hello@ryanconnaughton.com>
2022-11-10 18:09:53 +0000

    builder: added inital function logic with header test

--- src/build.js
+++ src/build.js
@@ -1,9 +1,52 @@
 // store the components
 const handlebars = require('handlebars');
+const getVarsFor = require('./getVars');
+const fs = require('fs');
+const path = require('path');
 
-console.log(handlebars);
+const config = [
+  {
+    page: 'home',
+    read: './components/header.hbs',
+    write: './somePath',
+    vars: {
+      userType: 'founder',
+      logoSrc: './assets/logo.svg',
+      primaryCtaTxt: 'Sign Up',
+      primaryCtaLink: 'https://app.tertle.io/join',
+      secondaryCtaTxt: 'Login',
+      secondaryCtaLink: 'https://app.tertle.io/login',
+      showFooterVisual: false,
+      useModal: false,
+    },
+  },
+];
 
-// 1. vars & components
-// 2. get pages
-// 3. compose template
-// 4. write html files int public
+class Builder {
+  constructor(config) {
+    this.config = config;
+  }
+
+  getFile(pathname) {
+    return fs.readFileSync(path.resolve(__dirname, pathname), 'utf-8');
+  }
+
+  compose(component, vars) {
+    console.log(vars);
+    const template = handlebars.compile(component);
+    const htmlFile = template({ ...vars });
+
+    return htmlFile;
+  }
+
+  // write file
+
+  run() {
+    const file = this.getFile(this.config[0].read);
+    const composed = this.compose(file, this.config[0].vars);
+    console.log(composed);
+  }
+}
+
+const builder = new Builder(config);
+builder.run();
