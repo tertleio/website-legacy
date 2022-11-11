ac58914beb55ec117ceddbe1e541d700ed1585c6
Ryan Connaughton <hello@ryanconnaughton.com>
2022-11-11 19:45:45 +0000

    add: build todo

--- src/build.js
+++ src/build.js
@@ -3,6 +3,10 @@ const Builder = require('./Builder');
 const config = require('./config');
 const builder = new Builder(config);
 
+// TODO:
+// only run build on files that have changed
+// https://www.codingninjas.com/codestudio/library/nodemon
+
 (() => {
   const len = config.length;
   for (let i = 0; i < len; i++) {
