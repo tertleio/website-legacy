5703ba7409809b404b555df117fe343315a77015
Ryan Connaughton <hello@ryanconnaughton.com>
2022-11-11 19:18:47 +0000

    minor cleanup from previous commit

--- src/utils/logs.js
+++ src/utils/logs.js
@@ -3,7 +3,7 @@ const Red = '\x1b[31m';
 const Green = '\x1b[32m';
 const Yellow = '\x1b[33m';
 
-const ylw = (str) => Yellow + `${str}` + Reset + '...';
-const grn = (str) => Green + `${str}` + Reset;
+const ylw = (str) => Yellow + str + Reset + '...';
+const grn = (str) => Green + str + Reset;
 
 module.exports = { ylw, grn };
