6a66ee33bd5bf3f8021362fce46aaf3d09777e4f
Ryan Connaughton <hello@ryanconnaughton.com>
2022-11-13 15:19:26 +0000

    add: parse meta data util

---
+++ src/utils/parseMeta.js
@@ -0,0 +1,35 @@
+const parseMeta = (file, tokens = ['---', '---']) => {
+  function getHeader(data) {
+    const strReg = '^' + tokens[0] + '([\\s|\\S]*?)' + tokens[1],
+      reg = new RegExp(strReg),
+      newFile = reg.exec(data);
+
+    return newFile ? newFile[1] : new Error('!parse header');
+  }
+
+  function getContent(data) {
+    const strReg = '^ *?\\' + tokens[0] + '[^]*?' + tokens[1] + '*',
+      reg = new RegExp(strReg),
+      content = data.replace(reg, '');
+
+    return content;
+  }
+
+  const header = getHeader(file);
+  const content = getContent(file);
+  return { meta: header, content: content };
+};
+
+module.exports = parseMeta;
+
+// function toJson(data) {
+//   const tmpObj = {},
+//     lines = data.trim().split('\n');
+
+//   lines.forEach(function (line, i) {
+//     const arr = line.trim().split(':');
+//     tmpObj[arr.shift()] = arr.join(':').trim();
+//   });
+
+//   return tmpObj;
+// }
