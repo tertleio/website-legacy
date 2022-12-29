1be0cf8d40db9367dbc6fe4c1e13da4c0d3b7dac
Ryan Connaughton <hello@ryanconnaughton.com>
2022-12-29 09:51:29 +0000

    features: update demo js to handle bar text on feature click

--- public/js/demo.js
+++ public/js/demo.js
@@ -1,26 +1,39 @@
 'use strict';
 const doc = document;
 
-function demo() {
-  // get all the features
-  const elContent = doc.querySelectorAll('.feature');
-  // console.log(elsDemo);
+const barTxt = {
+  s1: ['match', 'profiles'],
+  s2: ['introductions', 'filter'],
+  s3: ['schedule', 'message', 'dashboard'],
+};
 
-  elContent.forEach((c) => {
+function demo() {
+  const elsContent = doc.querySelectorAll('.feature');
+
+  elsContent.forEach((c) => {
+    const barArr = barTxt[c.id];
+    console.log(barTxt);
     const elsDemo = c.querySelectorAll('.demo__dt');
+    console.log('elsDemo', elsDemo);
 
     c.querySelectorAll('.content-btn').forEach((btn) => {
       btn.addEventListener('click', (e) => {
+        // get dom
+        const targetId = e.target.id.split('-')[1] - 1;
+        const targetEl = elsDemo[targetId];
         const elCurrActive = c.querySelector('.--active');
-        elCurrActive.classList.remove('--active');
+        const elBar = c.querySelector('.demo-head__bar span');
 
+        // update buttons
+        elCurrActive.classList.remove('--active');
         btn.classList.add('--active');
 
-        const targetId = e.target.id.split('-')[1];
-        const targetEl = elsDemo[targetId - 1];
-
+        // update demo image
         elsDemo.forEach((demo) => demo.classList.remove('--active'));
         targetEl.classList.add('--active');
+
+        // update demo bar text
+        elBar.innerHTML = barArr[targetId];
       });
     });
   });
